9b1881f8dd8d2398b5b39610be7bb6edb5fe74d8
feden2906 <feden2906@gmail.com>
2023-03-17 20:32:01 +0200

    hw7

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { authController } from "../controllers";
+import { EActionTokenType } from "../enums/action-token-type.enum";
 import {
   authMiddleware,
   commonMiddleware,
@@ -33,14 +34,28 @@ router.post(
 
 router.post(
   "/password/forgot",
+  commonMiddleware.isBodyValid(UserValidator.emailValidator),
   userMiddleware.getDynamicallyOrThrow("email"),
   authController.forgotPassword
 );
 
 router.put(
   `/password/forgot/:token`,
-  authMiddleware.checkActionForgotToken,
+  authMiddleware.checkActionToken(EActionTokenType.forgot),
   authController.setForgotPassword
+);
+
+router.post(
+  "/activate",
+  commonMiddleware.isBodyValid(UserValidator.emailValidator),
+  userMiddleware.getDynamicallyOrThrow("email"),
+  authController.sendActivateToken
+);
+
+router.put(
+  `/activate/:token`,
+  authMiddleware.checkActionToken(EActionTokenType.activate),
+  authController.activate
 );
 
 router.post(
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { EEmailActions } from "../enums";
+import { EEmailActions, EUserStatus } from "../enums";
 import { EActionTokenType } from "../enums/action-token-type.enum";
 import { ApiError } from "../errors";
 import { Action, Token, User } from "../models";
@@ -125,6 +125,37 @@ class AuthService {
       const hashedPassword = await passwordService.hash(password);
 
       await User.updateOne({ _id: id }, { password: hashedPassword });
+    } catch (e) {
+      throw new ApiError(e.message, e.status);
+    }
+  }
+
+  public async sendActivateToken(user: IUser): Promise<void> {
+    try {
+      const actionToken = tokenService.generateActionToken(
+        { _id: user._id },
+        EActionTokenType.activate
+      );
+      await Action.create({
+        actionToken,
+        tokenType: EActionTokenType.activate,
+        _user_id: user._id,
+      });
+
+      await emailService.sendMail(user.email, EEmailActions.ACTIVATE, {
+        token: actionToken,
+      });
+    } catch (e) {
+      throw new ApiError(e.message, e.status);
+    }
+  }
+
+  public async activate(userId: string): Promise<void> {
+    try {
+      await User.updateOne(
+        { _id: userId },
+        { $set: { status: EUserStatus.active } }
+      );
     } catch (e) {
       throw new ApiError(e.message, e.status);
     }
